3e879b94841869e0d0793dd86e7b8f0e518925b1
Muiz <folarinolaore93@gmail.com>
2023-09-09 09:23:47 -0700

    Modified file 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 # Function to print string in uppercase
 # Author - Muiz Olaore
+def islower(c):
+    if ord(c) >= ord('a') and ord (c) <= ord('z'):
+        return True
+    else:
+        return False
 
 def uppercase(str):
     for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            c = chr(ord(c) - 32)
-        print("{}".format(c), end="")
+        print("{:c}".format(ord(c) if not islower(c) else ord(c) - 32), end="")
     print("")
