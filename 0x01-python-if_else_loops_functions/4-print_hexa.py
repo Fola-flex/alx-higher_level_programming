3b762c2088ae88a57806b312acc7b946fb67c878
Muiz <folarinolaore93@gmail.com>
2023-09-09 05:02:48 -0700

    Modified files 2 and 3

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#Print all numbers from 0 to 98 in decimal and in hexadecimal
-#Author: Muiz Olaore
+# Print all numbers from 0 to 98 in decimal and in hexadecimal
+# Author: Muiz Olaore
 for number in range(0, 99):
     print("{} = {}".format(number, hex(number)))
