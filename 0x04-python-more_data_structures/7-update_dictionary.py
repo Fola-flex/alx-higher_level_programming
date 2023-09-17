27e0b9cc864be0b478a26e139f21904916e2955e
Muiz <folarinolaore93@gmail.com>
2023-09-17 02:53:17 -0700

    Modified file 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,11 +2,5 @@
 # Author: Muiz Olaore
 
 def update_dictionary(a_dictionary, key, value):
-    sorted_dic = sorted(a_dictionary)
-    for i in sorted_dic:
-        if i == key:
-            a_dictionary[i] = value
-        else:
-            a_dictionary[key] = value
-        print("{}: {}".format(i, a_dictionary[i]), end="\n")
-
+    a_dictionary[key] = value
+    return sorted(a_dictionary)
