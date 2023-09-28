9bf486f2eaec017995984ddf6e37e5e8a0b26f41
Muiz <folarinolaore93@gmail.com>
2023-09-27 20:15:15 -0700

    Created files and new directory 0x05

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author: Muiz Olaore
+
+def raise_exception():
+    """Raise a TypeError exception."""
+    raise TypeError
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+This is a directory containing functions and scriipts on exceptions in python
