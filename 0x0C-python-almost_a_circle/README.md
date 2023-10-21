290ca3e22fdf14f978871f80aae7d8e264fba775
Muiz <folarinolaore93@gmail.com>
2023-10-21 08:05:12 -0700

    Created directory 0x0C along with files

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,1 @@
+This is a directory containing functions, tests etc on everything previously learned in python
---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Base of all other classes"""
+
+
+class Base:
+    """Base model.
+
+    This is the base class for all other classes
+
+    Private Class Attributes:
+        __nb_object (int): Number of instantiated Bases.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new Base.
+
+        Args:
+            id (int): The identity of the new Base.
+
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON serialization of a list of dicts.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
