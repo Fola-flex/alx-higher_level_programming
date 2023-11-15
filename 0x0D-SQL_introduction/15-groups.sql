58e0f66e62dc7f558ba059b3edc3f3decb291778
Muiz <folarinolaore93@gmail.com>
2023-11-15 06:12:39 -0800

    Created files 10 -16

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- Lists the number od records with the same score in the table second_table
+-- hbtn_0c_0 and displays the score and number of records for this score 
+-- with the label number
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score;
