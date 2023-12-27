5239efb773ced80713573ab35a3433b210f6f96e
Muiz <folarinolaore93@gmail.com>
2023-12-26 23:46:07 -0800

    Corrected file 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,11 +6,10 @@ class Square extends oldSquare {
     if (c === undefined) {
       c = 'X';
     }
-
     for (let i = 0; i < this.height; i++) {
       let s = '';
       for (let j = 0; j < this.width; j++) {
-        s += 'c';
+        s += c;
       }
       console.log(s);
     }
