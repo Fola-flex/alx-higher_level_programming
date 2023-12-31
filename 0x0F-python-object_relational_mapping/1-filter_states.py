4483464cf4ef661f1dbe292e6cb033b02e0af67d
Muiz <folarinolaore93@gmail.com>
2023-12-31 08:24:08 -0800

    Set pycodestyle standard

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,9 +6,11 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1], 
-                        password=sys.argv[2], database=sys.argv[3], port=3306)
+                         password=sys.argv[2], database=sys.argv[3],
+                         port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id")
+    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%' 
+                ORDER BY states.id""")
     row = cur.fetchall()
     for row in rows:
         print(row)
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,7 +6,8 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1], 
-                        password=sys.argv[2], database=sys.argv[3], port=3306)
+                         password=sys.argv[2], database=sys.argv[3],
+                         port=3306)
     cur = db.cursor()
     match = sys.argv[4]
     cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
