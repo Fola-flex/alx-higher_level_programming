02089f51d2b8e4e57264dec566637f039ea9aa9a
Muiz <folarinolaore93@gmail.com>
2023-12-30 16:44:48 -0800

    Created files 0-14

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" list all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], password=sys.argv[2], database=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    row = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+This is a directory on object relational mapping in Python
