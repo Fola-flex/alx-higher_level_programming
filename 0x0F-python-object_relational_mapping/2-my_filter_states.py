b76a9036814119557a47cf534cdfeaa1ba6270b3
Muiz <folarinolaore93@gmail.com>
2023-12-31 09:16:22 -0800

    Corrected file 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" Lists all states from the database hbtn_0e_0_usa
-where name matched argiument passed """
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
@@ -10,8 +9,8 @@ if __name__ == "__main__":
                          password=sys.argv[2], database=sys.argv[3],
                          port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name
-                LIKE BINARY '{}'".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
